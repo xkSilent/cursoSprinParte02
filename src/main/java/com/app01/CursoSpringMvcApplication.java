6e49479e71700192e8b34fa7097f9fd0a4c9af30
xkSilent <fsdfelipe_dias@live.com>
2020-09-29 19:18:30 -0300

    Produto e associação Muitos para muitos

--- src/main/java/com/app01/CursoSpringMvcApplication.java
+++ src/main/java/com/app01/CursoSpringMvcApplication.java
@@ -8,13 +8,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.app01.domain.Categoria;
+import com.app01.domain.Produto;
 import com.app01.repositories.CategoriaRepository;
+import com.app01.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursoSpringMvcApplication implements CommandLineRunner {
 	
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	@Autowired
+	private ProdutoRepository produtoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursoSpringMvcApplication.class, args);
@@ -27,8 +31,25 @@ public class CursoSpringMvcApplication implements CommandLineRunner {
 		Categoria cat2 = new Categoria();
 		cat2.setNome("Escritório");
 		
-		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		Produto p1 = new Produto();
+		p1.setNome("Computador");
+		p1.setPreco(2000.00);
+		Produto p2 = new Produto();
+		p2.setNome("Impressora");
+		p2.setPreco(800.00);
+		Produto p3 = new Produto();
+		p3.setNome("Mouse");
+		p3.setPreco(80.00);
 		
+		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
 	}
 
 
--- src/main/java/com/app01/domain/Produto.java
+++ src/main/java/com/app01/domain/Produto.java
@@ -8,27 +8,34 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
 @Entity
-public class Categoria implements Serializable {
+public class Produto implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int id;
 	private String nome;
+	private double preco;
 	
-	@ManyToMany(mappedBy="categorias")
-	private List<Produto> produtos = new ArrayList<>();
-	public Categoria(){
-		
-	}
+	@ManyToMany
+	@JoinTable(name = "CATEGORIA_PRODUTO",
+	joinColumns = @JoinColumn(name = "produto_id"),
+	inverseJoinColumns = @JoinColumn(name = "categoria_id")
+	)
+	private List<Categoria> categorias = new ArrayList<>();
+	
+	public Produto() {}
 
-	public Categoria(int id, String nome) {
+	public Produto(int id, String nome, double preco) {
 		super();
 		this.id = id;
 		this.nome = nome;
+		this.preco = preco;
 	}
 
 	public int getId() {
@@ -46,14 +53,21 @@ public class Categoria implements Serializable {
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	
 
-	public List<Produto> getProdutos() {
-		return produtos;
+	public double getPreco() {
+		return preco;
 	}
 
-	public void setProdutos(List<Produto> produtos) {
-		this.produtos = produtos;
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	public List<Categoria> getCategorias() {
+		return categorias;
+	}
+
+	public void setCategorias(List<Categoria> categorias) {
+		this.categorias = categorias;
 	}
 
 	@Override
@@ -61,7 +75,6 @@ public class Categoria implements Serializable {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + id;
-		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
 		return result;
 	}
 
@@ -73,16 +86,12 @@ public class Categoria implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Categoria other = (Categoria) obj;
+		Produto other = (Produto) obj;
 		if (id != other.id)
-			return false;
-		if (nome == null) {
-			if (other.nome != null)
-				return false;
-		} else if (!nome.equals(other.nome))
 			return false;
 		return true;
 	}
+	
 	
 
 }
