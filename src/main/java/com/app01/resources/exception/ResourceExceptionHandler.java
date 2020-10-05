b7f8201537374365cee8cd2f1cc3b551ac9d1399
xkSilent <fsdfelipe_dias@live.com>
2020-10-05 20:52:42 -0300

    Deletando uma Categoria com DELETE

--- src/main/java/com/app01/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/com/app01/resources/exception/ResourceExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.app01.services.exceptions.DataIntegrityException;
 import com.app01.services.exceptions.ObjectNotFoundException;
 
 @ControllerAdvice
@@ -19,6 +20,15 @@ public class ResourceExceptionHandler{
 				e.getMessage(), System.currentTimeMillis());
 		
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(erro);
+	}
+	
+	@ExceptionHandler(DataIntegrityException.class)
+	public ResponseEntity<StandardError> dataIntegrity (DataIntegrityException e,
+			HttpServletRequest request){
+		StandardError erro = new StandardError(HttpStatus.BAD_REQUEST.value(),
+				e.getMessage(), System.currentTimeMillis());
+		
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(erro);
 	}
 	
 }
--- src/main/java/com/app01/services/CategoriaService.java
+++ src/main/java/com/app01/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.app01.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.app01.domain.Categoria;
 import com.app01.repositories.CategoriaRepository;
+import com.app01.services.exceptions.DataIntegrityException;
 import com.app01.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -28,6 +30,15 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+		repo.deleteById(id);
+		}catch(DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma Categoria que possui produtos");
+		}
 	}
 
 }
