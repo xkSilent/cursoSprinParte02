1e8cc64a86a88228be6bd63264e147ea48f03335
xkSilent <fsdfelipe_dias@live.com>
2020-10-09 20:57:09 -0300

    Correções: update em CategoriaService e construtor de Pagamento

--- src/main/java/com/app01/services/CategoriaService.java
+++ src/main/java/com/app01/services/CategoriaService.java
@@ -33,8 +33,9 @@ public class CategoriaService {
 	}
 	
 	public Categoria update(Categoria obj) {
-		find(obj.getId());
-		return repo.save(obj);
+		Categoria newObj =find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
 	}
 	
 	public void delete(Integer id) {
@@ -61,6 +62,10 @@ public class CategoriaService {
 		Categoria cat = new Categoria();
 		cat.setNome(objDto.getNome());
 		return cat;
+	}
+	
+	public void updateData(Categoria newObj, Categoria obj) {
+		newObj.setNome(obj.getNome());
 	}
 
 }
