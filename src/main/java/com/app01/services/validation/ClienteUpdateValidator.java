d0f28779ed8e9b5a7897d9bf2793f8f9be3ee2f3
xkSilent <fsdfelipe_dias@live.com>
2020-10-11 20:26:02 -0300

    Melhoria validação: email não repetido na atualização do cliente

--- src/main/java/com/app01/services/validation/ClienteUpdateValidator.java
+++ src/main/java/com/app01/services/validation/ClienteUpdateValidator.java
@@ -14,39 +14,23 @@ import com.app01.repositories.ClienteRepository;
 import com.app01.resources.exception.FieldMessage;
 
 public class ClienteUpdateValidator implements ConstraintValidator<ClienteUpdate, ClienteDTO>{
-	
+
 	public void initialize(ClienteUpdate ann) {}
-	
+
 	@Autowired
 	private ClienteRepository repo;
-	
-<<<<<<< HEAD
-	@Autowired
-	private HttpServletRequest request;
-=======
->>>>>>> 67cd766... Validação customizada: email não repetido na atualização de cliente
+
 
 	@Override
 	public boolean isValid(ClienteDTO objDto, ConstraintValidatorContext context) {
-		
-<<<<<<< HEAD
-		Map<String, String> map = (Map<String, String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
-		
-		Integer uriId = Integer.parseInt(map.get("id"));
-		
+
 		List<FieldMessage> list = new ArrayList<>();
-		
-		Cliente aux = repo.findByEmail(objDto.getEmail());
-		if(aux != null && !Integer.valueOf(aux.getId()).equals(uriId)) {
-=======
-		List<FieldMessage> list = new ArrayList<>();
-		
+
 		Cliente aux = repo.findByEmail(objDto.getEmail());
 		if(aux != null && aux.getEmail() != objDto.getEmail()) {
->>>>>>> 67cd766... Validação customizada: email não repetido na atualização de cliente
 			list.add(new FieldMessage("email", "email já existente"));
 		}
-		
+
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();
 			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName()).addConstraintViolation();
