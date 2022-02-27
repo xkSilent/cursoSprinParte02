095caa7221e956c274fdafd9da323fe182c2a5bc
xkSilent <fsdfelipe_dias@live.com>
2022-02-27 00:41:57 -0300

    Salvando primeiro arquivo no S3 Amazon

--- src/main/java/com/app01/CursoSpringMvcApplication.java
+++ src/main/java/com/app01/CursoSpringMvcApplication.java
@@ -1,11 +1,17 @@
 package com.app01;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.app01.services.S3Service;
+
 @SpringBootApplication
 public class CursoSpringMvcApplication implements CommandLineRunner {
+	
+	@Autowired
+	private S3Service s3Service;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursoSpringMvcApplication.class, args);
@@ -13,6 +19,7 @@ public class CursoSpringMvcApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {	
+		s3Service.uploadfile("C:\\Users\\fsdfe\\OneDrive\\Pictures\\bitcoin nft.png");
 	}	
 
 }
